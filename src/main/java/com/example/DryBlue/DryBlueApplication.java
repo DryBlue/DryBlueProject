c209d0dee3110a6b35f2a072f1baeb18fc1716d9
Stefanomun <mungiellostefano3@gmail.com>
2021-12-30 19:18:00 +0100

    initializer

---
+++ src/main/java/com/example/DryBlue/DryBlueApplication.java
@@ -0,0 +1,13 @@
+package com.example.DryBlue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DryBlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DryBlueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DryBlue/DryBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DryBlue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DryBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
