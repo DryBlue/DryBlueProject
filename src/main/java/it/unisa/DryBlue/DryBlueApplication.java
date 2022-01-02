412bb2e6c704a572a1f3619330ab8150fc92f3f1
pierago01 <pierago20@gmail.com>
2022-01-02 10:55:35 +0100

    Created Database

--- src/main/java/it/unisa/DryBlue/DryBlueApplication.java
+++ src/main/java/it/unisa/DryBlue/DryBlueApplication.java
@@ -1,4 +1,4 @@
-package com.example.DryBlue;
+package it.unisa.DryBlue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/it/unisa/DryBlue/DryBlueApplicationTests.java
+++ src/test/java/it/unisa/DryBlue/DryBlueApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.DryBlue;
+package it.unisa.DryBlue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
